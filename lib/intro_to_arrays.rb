7702efd177acef07db20f28ad21cfc8651996053
Kiley Bobbitt <jkileybob@aol.com>
2018-11-13 20:22:12 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,6 +1,21 @@
 def instantiate_new_array
   new_array = []
 end
+
 def array_with_two_elements
   new_with_two_array = ["Ted", "Kitten"]
 end  
+
+def first_element(array)
+  array[0]
+end  
+
+def third_element(array)
+  array[2]
+end
+
+def last_element(array)
+  array.last
+end
+
+    
